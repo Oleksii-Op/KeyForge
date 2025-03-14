4f62e57de14138da08c0716421008dd8869e5f28
Oleksii-Op <oleksii.oprish@gmail.com>
2025-03-14 17:33:01 +0200

    Add max payload validations

--- application/core/schemas.py
+++ application/core/schemas.py
@@ -10,12 +10,15 @@ class PublicKeyOut(BaseModel):
 
 
 class PasswordIn(BaseModel):
-    password: str | None = None
+    password: str | None = Field(
+        None,
+        max_length=128,
+        min_length=0,
+    )
 
 
-class PrivateKeyIn(BaseModel):
-    public_key: str
-    password: str | None = None
+class PrivateKeyIn(PasswordIn):
+    private_key: str
 
 
 class HashOut(BaseModel):
@@ -23,16 +26,32 @@ class HashOut(BaseModel):
 
 
 class BasePayload(BaseModel):
-    payload: str | None
+    payload: str | None = Field(
+        default=None,
+        max_length=256,
+        min_length=0,
+    )
 
 
 class Argon2HashParams(BasePayload):
-    length: int = Field(default=32, ge=8, le=32)
-    memory_cost: int = Field(default=65536, ge=8, le=244141)
+    length: int = Field(
+        default=32,
+        ge=8,
+        le=32,
+    )
+    memory_cost: int = Field(
+        default=65536,
+        ge=8,
+        le=244141,
+    )
 
 
 class BcryptHashParams(BasePayload):
-    rounds: int = Field(default=12, ge=8, le=32)
+    rounds: int = Field(
+        default=12,
+        ge=8,
+        le=32,
+    )
 
 
 class FileHashedResponse(BaseModel):
